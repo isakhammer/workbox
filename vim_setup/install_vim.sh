650c97bfa2cec14810c5fb8e29c3f44051246628
isakhammer <isakhammer@gmail.com>
2022-03-28 23:35:14 +0200

    Updated coc installations

--- vim_setup/install_vim.sh
+++ vim_setup/install_vim.sh
@@ -5,5 +5,7 @@ mkdir -p ~/.config/nvim/ && touch ~/.config/nvim/init.vim && echo "source ~/work
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
- nvim --headless +PlugInstall +qall
+nvim --headless +PlugInstall +qall
 
+nvim +'CocInstall -sync coc-julia coc-jedi coc-json' +qall
+nvim +CocUpdateSync +qall
